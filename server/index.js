7f3799b494e5e2a9ac08f3d39599dd8e97f5b9ab
Cole Callaway <nccallaway54@gmail.com>
2022-04-08 14:26:04 -0600

    test

--- server/index.js
+++ server/index.js
@@ -3,15 +3,15 @@ const cors = require("cors");
 
 const app = express();
 
-
 app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
 app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
+  const compliments = [
+    "Gee, you're a smart cookie!",
+    "Cool shirt!",
+    "Your Javascript skills are stellar.",
   ];
 
   // choose random compliment
@@ -19,7 +19,22 @@ app.get("/api/compliment", (req, res) => {
   let randomCompliment = compliments[randomIndex];
 
   res.status(200).send(randomCompliment);
-  
+});
+
+app.get("/api/fortune", (req, res) => {
+  const fortune = [
+    "A friend is a present you give yourself.",
+    "A golden egg of opportunity falls into your lap this month.",
+    "A faithful friend is a strong defense.",
+    "A hunch is creativity trying to tell you something.",
+    "A lifetime friend shall soon be made.",
+  ];
+
+  //random fortune
+  let randomFortuneIndex = Math.floor(Math.random() * fortune.length);
+  let randomFortune = fortune[randomFortuneIndex];
+
+  res.status(200).send(randomFortune);
 });
 
 app.listen(4000, () => console.log("Server running on 4000"));
